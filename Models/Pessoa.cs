ef44a0e410a3014192cbf341a9ecc14d4a53e3b3
Murilo Pedro Galassi <murilo.galassi@educacao.sp.gov.br>
2024-11-08 16:15:26 -0300

    Upload

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -7,8 +7,39 @@ namespace ExemploExplorando.Models
 {
     public class Pessoa
     {
-        public string Nome { get; set; }
-        public int Idade { get; set; }
+        private string _nome;
+        private int _idade;
+
+        public string Nome 
+        { 
+            get => _nome.ToUpper();
+
+            set
+            {
+                if(value == "")
+                {
+                    throw new ArgumentException("O nome não pode ser vazio");
+                }
+
+                _nome = value;
+            }
+        }
+
+
+        public int Idade
+        { 
+            get => _idade;
+
+            set
+            {
+                if(value <= 0)
+                {
+                    throw new ArgumentException("A idade não pode ser menor que 0");
+                }
+
+                _idade = value;
+            }
+        }
 
         public void Apresentar()
         {
